c2bd3ef8e5283e12b8a79adebe12c2888f3e311e
your-name <your>
2023-12-28 20:34:11 +0800

    完成猜數字區間遊戲

--- game.py
+++ game.py
@@ -6,15 +6,27 @@ import random
 # 猜對提前離開
 # 猜錯,提前離開
 
-x = random.randint(1, 50)
+
+start = 1
+end = 50
+x = random.randint(start, end)
+
 print(x)
 for i in range(5):
-    y = int(input("請猜數字(1~50)"))
+    y = int(input(f"請猜數字({start}~{end})"))
     if y == x:
         print("猜對了^^~~")
         break
     else:
         if y > x:
             print("猜低一點!")
+            if end > y:
+                end = y - 1
         else:
+            if start < y:
+                start = y + 1
             print("猜高一點了~~!")
+if y != x:
+    print(f"猜錯了!!答案為:{x}")
+else:
+    print(f"共猜{i+1}次")
